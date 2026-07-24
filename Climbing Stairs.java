66da220148d33e145d1a5ce4d86fec301cc0389b
Sohom Roy <rsohom426@gmail.com>
2026-07-24 22:47:43 +0530

    Leetcode Problem 70 solution added

---
+++ Climbing Stairs.java
@@ -0,0 +1,20 @@
+// Leetcode Problem 70: Climbing Stairs
+// JAVA CODE
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int first = 1;
+        int second = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int current = first + second;
+            first = second;
+            second = current;
+        }
+
+        return second;
+    }
+}

3f45b7b6c8b91dd10b1f37695461cf9e109d2353
Sohom Roy <rsohom426@gmail.com>
2026-08-01 21:59:22 +0530

    Leetcode Problem 80 solution added

---
+++ Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,20 @@
+// Leetcode Problem 80: Remove Duplicates from Sorted Array II
+// JAVA CODE
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+
+        int k = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
---
+++ Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,15 @@
+# Leetcode Problem 80: Remove Duplicates from Sorted Array II
+# PYTHON CODE
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return len(nums)
+
+        k = 2
+
+        for i in range(2, len(nums)):
+            if nums[i] != nums[k - 2]:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
